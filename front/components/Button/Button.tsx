3597182707f400727c2d4574e773f89ddcd029a8
gilvan-almeida <gilvanalmeidaaraujo748@gmail.com>
2026-04-10 17:10:19 -0300

    FEAT: button component

---
+++ front/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+function Button(){
+    return(
+        <button className="cursor-pointer px-6 py-2 rounded-full bg-[#00633b] font-medium">
+            <span>
+                Criar Conta
+            </span>
+        </button>
+    )
+}
+
+export default Button;
